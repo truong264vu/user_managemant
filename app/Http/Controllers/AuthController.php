97373b879fd23590e254dddeaf8dfdc338060644
Vũ Xuân Trường <70488183+truong264vu@users.noreply.github.com>
2021-12-21 20:02:05 +0700

    update

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function UserRegister(){
+        return 'a';
+    }
+
+    public function UserLogin(){
+        return 'b';
+    }
+}
